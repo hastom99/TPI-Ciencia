e0d4d8f47f376a18fb15c6874649ab49f26ce859
Thomas <36000258+hastom99@users.noreply.github.com>
2021-11-08 21:51:51 -0300

    delete comments (#5)
    Co-authored-by: Thomas <thomas.munoz@mercadolibre.com>

--- main.cpp
+++ main.cpp
@@ -6,11 +6,3 @@ int main(int argc, char **argv) {
     testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
 }
-<<<<<<< HEAD
-//first commit
-=======
-//un comentario diferente
->>>>>>> 7e61ecc86db80a4a2886565f0c1aa3521629e2f5
-
-// First commit tobias.
-// Second commit.
