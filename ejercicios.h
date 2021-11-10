32aa9be777da1fa97dc44a61f1a3a9e8a87f9a17
Tobias Munoz <munoztobias00@gmail.com>
2021-11-10 00:06:44 -0300

    Subo ejercicio1 completo

--- ejercicios.h
+++ ejercicios.h
@@ -10,6 +10,27 @@
 using namespace std;
 
 bool esEncuestaValida ( eph_h th, eph_i ti );
+bool esValida ( eph_h th, eph_i ti );
+bool vacia(eph_h th);
+bool esMatriz(vector<vector<int> > m);
+bool cantidadCorrectaDeColumnasI(eph_i ti);
+bool cantidadCorrectaDeColumnasH(eph_h th);
+bool hayIndividuosSinHogares(eph_h th, eph_i ti);
+bool hayHogaresSinIndividuos(eph_h th, eph_i ti);
+bool hayRepetidosI(eph_i ti);
+bool hayRepetidosH(eph_i ti);
+bool mismoAnioYTrimestre(eph_i ti, eph_h th);
+bool menosDe21MiembrosPorHogar(eph_h th, eph_i ti);
+int cantHabitantes(hogar h, eph_i ti);
+bool esSuHogar(hogar h, individuo i);
+bool cantidadValidaDormitorios(eph_h th);
+bool valoresEnRangoI(eph_i ti);
+bool individuoValido(individuo i);
+bool valoresEnRangoH(eph_h th);
+bool hogarValido(hogar h);
+bool valorRegionValido(int r);
+bool mismoCodUsuYComponente(individuo i1, individuo i2);
+
 vector < int > histHabitacional ( eph_h th, eph_i ti, int region );
 vector < pair < int, float > > laCasaEstaQuedandoChica ( eph_h th, eph_i ti );
 bool creceElTeleworkingEnCiudadesGrandes ( eph_h t1h, eph_i t1i, eph_h t2h, eph_i t2i );
